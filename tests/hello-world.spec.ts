7ac6f6c8f45d1cf78062237add4f03fdfc0b60b8
Lucas Alexandre <lucasalexandre.s.cc@gmail.com>
2024-01-20 16:38:11 -0300

    chore: try to test husky

--- tests/hello-world.spec.ts
+++ tests/hello-world.spec.ts
@@ -3,7 +3,7 @@ import { helloWorld } from '@/index';
 describe('Index', () => {
   describe('helloWorld', () => {
     it("returns 'Hello world!'", () => {
-      expect(helloWorld()).toEqual('Hello world!');
+      expect(helloWorld()).toEqual('Hello world! 2');
     });
   });
 });
